2dc2b13efb53e3a42475bb675270af5fa0db351e
moldye <yummybreakfast46@gmail.com>
2022-09-09 15:52:05 +0000

    added lmao trying to learn coding moment text

--- test.java
+++ test.java
@@ -4,6 +4,7 @@ class Main {
     System.out.println("hi");
     System.out.println("hi again");
     System.out.println("hi again again");
-    System.out.println("hi again again");
+    System.out.println("hi again again again");
+    System.out.println("lmao trying to learn coding moment");
   }
 }
